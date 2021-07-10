31bd94df3c717ed4acfb2cac335cbaeef06c88c5
Sun Kim <sunkim0205@gmail.com>
2021-07-10 10:40:33 -0500

    setup routes and server file

---
+++ server/index.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+let port = process.env.PORT || 4000;
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.route();
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`)
+})
