ffb1566a69c20e8eb03e0cc35d7d7a19d65a8732
Christian Johansen <cjohansen11@me.com>
2021-07-12 17:34:56 -0400

    basic items added to transaction modal with unfinished css

--- client/index.js
+++ client/index.js
@@ -2,4 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/App.jsx";
 
-ReactDOM.render(<App />, document.getElementById("app"));
+import TransactionModal from "./src/components/TransactionModal.jsx";
+
+ReactDOM.render(<TransactionModal />, document.getElementById("app"));
+// ReactDOM.render(<App />, document.getElementById("app"));
