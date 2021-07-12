cc5bec6ab9e0ba39f501dbc43e96c5c1ed344c72
Alyssa Antonaccio <alyantonaccio@gmail.com>
2021-07-12 18:14:48 -0500

    Added login

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,56 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Link } from "react-router-dom";
+import axios from 'axios';
 import MarketplacePage from "./pages/MarketplacePage.jsx";
 import UserPage from "./pages/UserPage.jsx";
 import SplashPage from "./pages/SplashPage.jsx";
 
 const App = () => {
+
+  const [currentUser, setCurrentUser] = useState({
+    "user_id": "60ec7f5530509e56fa050fe7",
+    "results": [
+        {
+            "ratings_reviews": [],
+            "transactions": [],
+            "_id": "60ec7f5530509e56fa050fe7",
+            "firstName": "Scotty",
+            "lastName": "Scottman",
+            "email": "meowmeow@meow.com",
+            "phone": 2223334444,
+            "location": "77388",
+            "createdAt": "2021-07-12T17:43:49.789Z",
+            "updatedAt": "2021-07-12T17:52:01.789Z",
+            "__v": 0
+        }
+    ],
+    isUser: false
+   }
+   );
+   const [isLoggedIn, setIsLoggedIn] =useState(false);
+
+  const getUserInfo = () => {
+    axios.get('/google')
+    .then((data) => {
+      if (data.isUser) {
+        setIsLoggedIn(true);
+        setCurrentUser(data);
+      } else {
+        setCurrentUser(data);
+      }
+    })
+
+  };
+
   return (
     <Router>
       <Switch>
         <Route exact path="/">
-          <SplashPage />
+          <SplashPage
+          getUserInfo={getUserInfo}
+          isLoggedIn={isLoggedIn}
+          currentUser={currentUser}
+          />
         </Route>
         <Route path="/user">
           <UserPage />
--- client/src/pages/SplashPage.jsx
+++ client/src/pages/SplashPage.jsx
@@ -1,7 +1,62 @@
-import React from "react";
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch, Redirect} from "react-router-dom";
+import axios from 'axios';
 
-const SplashPage = () => {
-  return <div>Splash</div>;
+const SplashPage = ({ getUserInfo, isLoggedIn, currentUser }) => {
+  const [firstName, setFirstName] = useState('');
+  const [lastName, setLastName] = useState('');
+  const [phone, setPhone] = useState('');
+  const [location, setLocation] = useState('');
+
+  const postNewUser = (e) => {
+    e.preventDefault();
+    axios.post('/user', {
+      firstName: firstName,
+      lastName: lastName,
+      email: currentUser.email,
+      phone: phone,
+      location: location
+    })
+  }
+
+
+  return (
+    <div>
+    {!isLoggedIn && (
+        <div>
+          <h2>Please Login</h2>
+          <button onClick={getUserInfo}>Login Using Google</button>
+        </div>
+      )
+    }
+    {isLoggedIn && !currentUser.isUser && (
+      <form onSubmit={postNewUser}>
+        <label>
+          First Name:
+          <input type='text' value={currentUser.results[0].firstName} required onChange={(e) => {setFirstName(e.target.value)}}/>
+        </label>
+        <label>
+          Last Name:
+          <input type='text' value={currentUser.results[0].lastName} required onChange={(e) => {setLastName(e.target.value)}}/>
+        </label>
+        <label>
+          Email:
+          <input type='email' value={currentUser.results[0].email} disabled/>
+        </label>
+        <label>
+          Phone Number:
+          <input type='tel' placeHolder='ex: 123-456-7890' pattern='[0-9]{3}-[0-9]{3}-[0-9]{4}' required onChange={(e) => {setPhone(e.target.value)}}/>
+        </label>
+        <label>
+          Zip Code:
+          <input type='number' placeHolder='ex: 33713' required onChange={(e) => {setLocation(e.target.value)}}/>
+        </label>
+        <input type='submit' value='Submit' />
+      </form>
+    )}
+    {isLoggedIn && currentUser.isUser && (<Redirect to="/marketplace"/>)}
+    </div>
+  )
 };
 
 export default SplashPage;
