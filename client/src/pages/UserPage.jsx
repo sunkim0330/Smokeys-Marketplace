a6e67983b7003fde3df0298915b74e273e682bda
Scott Provence <scott.provence@yahoo.com>
2021-07-12 13:24:58 -0400

    Basic Overview layout

--- client/src/pages/UserPage.jsx
+++ client/src/pages/UserPage.jsx
@@ -1,7 +1,22 @@
 import React from "react";
+import Overview from "../components/Overview/Overview.jsx";
 
 const UserPage = () => {
-  return <div>User</div>;
+  return (
+    <div className="user-page-container">
+      <h1>Welcome, Scott!</h1>
+      <section>
+        <ul className="tab-list">
+          <li className="tab">Overview</li>
+          <li className="tab">Current Trades</li>
+          <li className="tab">Past Trades</li>
+          <li className="tab">Reviews/Ratings</li>
+          <li className="tab">Add New Item</li>
+        </ul>
+        <Overview />
+      </section>
+    </div>
+  );
 };
 
 export default UserPage;
