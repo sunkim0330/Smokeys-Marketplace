3d6d60b24943ffac58127110ae39dc0cc538adea
Christian Johansen <cjohansen11@me.com>
2021-07-12 18:32:52 -0400

    transaction_modal

--- client/index.js
+++ client/index.js
@@ -2,7 +2,4 @@ import React from "react";
 import ReactDOM from "react-dom";
 import App from "./src/App.jsx";
 
-import TransactionModal from "./src/components/TransactionModal.jsx";
-
-ReactDOM.render(<TransactionModal />, document.getElementById("app"));
-// ReactDOM.render(<App />, document.getElementById("app"));
+ReactDOM.render(<App />, document.getElementById("app"));
