aa2d7575605a8f80dd4bdb910a4194026a865fc7
Matt Jennings <mjenningscba@gmail.com>
2021-07-14 15:50:27 -0400

    Form submission for new item, no image functionality yet

--- client/src/components/AddNewItem/AddNewItem.jsx
+++ client/src/components/AddNewItem/AddNewItem.jsx
@@ -1,17 +1,17 @@
 import React, { useState, useEffect } from "react";
-
+import axios from 'axios'
 const AddNewItem = ({ currentUser }) => {
   const [name, setName] = useState('')
-  const [type, setType] = useState('')
+  const [type, setType] = useState('goods')
   const [description, setDescription] = useState('')
 
   const submitItem = () => {
-    axios.post(`/items/${currentUser._id}`, 
-    {
+    let body = ({
       name: name,
       type: type,
       description: description
     })
+    axios.post(`/items/${currentUser._id}`, body)
     .then(res => console.log(res))
   }
 
@@ -20,21 +20,21 @@ const AddNewItem = ({ currentUser }) => {
       <h3>What are you trading?</h3>
       <div className="add-new-item-form">
         <h4>Listing Title</h4>
-        <input onChange={setName} type="text" />
+        <input onChange={e => setName(e.target.value)} type="text" />
         <h4>Type</h4>
-        <select onChange={setType} name="" id="">
-          <option value="product">Product</option>
+        <select onChange={e => setType(e.target.value)} name="" id="">
+          <option value="goods">Goods</option>
           <option value="service">Service</option>
         </select>
         <h4>Description</h4>
-        <textarea onChange={setDescription} cols="110" rows="15"></textarea>
+        <textarea onChange={e => setDescription(e.target.value)} cols="110" rows="15"></textarea>
         <h4>Add Photos</h4>
         <div className="new-item-photo">
           <div className="new-item-photo-add">+</div>
           <div>Upload Photo</div>
         </div>
       </div>
-      <button className="add-item-btn">Add Item</button>
+      <button onClick={submitItem} className="add-item-btn">Add Item</button>
     </div>
   );
 };
