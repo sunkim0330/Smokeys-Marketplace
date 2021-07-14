7fe0c1659b0da7a79a17c7a73b1e9d0a5804619d
Katie <katieqgriffith@gmail.com>
2021-07-14 14:17:52 -0400

    Implemented functionality of Ratings and Reviews React Component

--- client/src/components/RatingsReviews/RatingsReviews.jsx
+++ client/src/components/RatingsReviews/RatingsReviews.jsx
@@ -2,14 +2,14 @@ import React from "react";
 import RatingsReviewsList from "./RatingsReviewsList.jsx";
 
 const RatingsReviews = () => {
+
   return (
     <div className="ratings-reviews-container">
       <h3>Past Reviews</h3>
       <div className="ratings-reviews-list-container">
         <div className="ratings-reviews-list-container-header">
+          <div>Transaction ID</div>
           <div>Date</div>
-          <div>Traded for</div>
-          <div>Item Traded</div>
           <div>Rating</div>
           <div>Review</div>
         </div>
--- client/src/components/RatingsReviews/RatingsReviewsList.jsx
+++ client/src/components/RatingsReviews/RatingsReviewsList.jsx
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import RatingReviewsItem from "./RatingsReviewsItem.jsx";
+import axios from 'axios';
 
 const RatingReviewsList = () => {
+  const [reviews, setReviews] = useState(null);
+
+  const id = '60eac1d6a0e0293f05e414cf';
+  const getReviews = (id) => {
+    axios.get(`/reviews/${id}`)
+      .then(({data}) => {
+        setReviews(data);
+      })
+  }
+  useEffect(() => {
+    getReviews(id);
+  }, [])
+
   return (
     <div>
-      <RatingReviewsItem />
-      <RatingReviewsItem />
-      <RatingReviewsItem />
-      <RatingReviewsItem />
-      <RatingReviewsItem />
+      { reviews && reviews.results.map((review) => {
+          return <RatingReviewsItem
+          key={review.transaction_id}
+          date={review.createdAt}
+          transaction_id={review.transaction_id}
+          rating={review.ratings}
+          review={review.reviews}
+          />
+        })
+      }
     </div>
   );
 };
